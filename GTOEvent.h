eb09372e176417023b5d377533fa461563fc6a69
eabartlett <eabartlett@berkeley.edu>
2014-07-19 18:36:55 -0700

    Event

--- GTOEvent.h
+++ GTOEvent.h
@@ -7,7 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "PubNub.h"
+#import "GTOUser.h"
 
 @interface GTOEvent : NSObject
+
+@property (readonly, nonatomic) NSString *channel;
+@property (readonly, nonatomic) NSString *description;
+@property (readonly, nonatomic) NSString *location;
+@property (readonly, nonatomic) NSDate *createdDate;
+@property (readonly, nonatomic) GTOUser *user;
+
+- (instancetype) initWithDictionary:(NSDictionary*)dict;
+
+- (void) sendEvent;
 
 @end
--- GTOEvent.m
+++ GTOEvent.m
@@ -8,6 +8,37 @@
 
 #import "GTOEvent.h"
 
+@interface GTOEvent()
+
+@property (readwrite, nonatomic) NSString *channel;
+@property (readwrite, nonatomic) NSString *description;
+@property (readwrite, nonatomic) NSString *location;
+@property (readwrite, nonatomic) NSDate *createdDate;
+@property (readwrite, nonatomic) GTOUser *user;
+
+@end
+
 @implementation GTOEvent
+
+- (instancetype) initWithDictionary:(NSDictionary *)dict
+{
+    self = [super init];
+    
+    if (self) {
+        _channel = dict[@"channel"];
+        _description = dict[@"description"];
+        _location = dict[@"description"];
+        _createdDate = dict[@"date"];
+        _user = dict[@"user"];
+    }
+    return self;
+}
+
+- (void) sendEvent
+{
+    
+}
+
+
 
 @end
